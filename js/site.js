9543b23b5f0f87d18f35a2c766954015611f5ca9
Thomas Thurzo <thomas.thurzo@gmail.com>
2022-03-30 11:14:57 +0200

    Diverse Änderungen

--- js/site.js
+++ js/site.js
@@ -2,16 +2,19 @@
 // Variablen 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let rows = 10;
-let cols = 10;
-let snake;
+let rows = 20;
+let cols = 20;
+let snake = [ {x:19, y:3} ]
 let food;
 let cellWidth = canvas.width / cols;
 let cellHeight = canvas.height / rows;
 let direction = "LEFT";
 
+placeFood();
 
-// Aufruf der Funktionen
+setInterval(gameLoop, 500);
+document.addEventListener("keydown", keyMove);
+
 draw();
 
 
@@ -20,20 +23,67 @@ function draw(){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
-    ctx.fillRect(100, 150, 30 -1 , 30 -1);
-    ctx.fillRect(130, 150, 30 -1, 30 -1);
-    ctx.fillRect(160, 150, 30 -1, 30 -1);
-    ctx.fillRect(190, 150, 30 -1, 30 -1);
+   
+    snake.forEach(part => add(part.x, part.y));
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(290, 250, 30 -1, 30 -1);
+    ctx.fillStyle = "yellow";
+    add(food.x, food.y); 
+
+    requestAnimationFrame(draw);
 }
 
 function add(x, y){
-    ctx.fillRect(x, y, 30 - 1, 30 -1);
+    ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth - 1, cellHeight -1);
 }
 
 function gameLoop(){
+    if(direction == "LEFT"){
+        snake[0].x--;
+    }
+
+    if(direction == "RIGHT"){
+        snake[0].x++;
+    }
+
+    if(direction == "UP"){
+        snake[0].y--;
+    }
+
+    if(direction == "DOWN"){
+        snake[0].y++;
+    }
+
+    if(snake[0].x == food.x && snake[0].y == food.y){
+        // Futter einsammeln
+
+        placeFood();
+    }
+    
+}
+
+function keyMove(e) {
+    if(e.keyCode == 37){
+        direction = "LEFT";
+    }
+
+    if(e.keyCode == 38){
+        direction = "UP";
+    }
+
+    if(e.keyCode == 39){
+        direction = "RIGHT";
+    }
+
+    if(e.keyCode == 40){
+        direction = "DOWN";
+    }
+}
+
+function placeFood() {
+    let randomX = Math.floor(Math.random() * cols);
+    let randomY = Math.floor(Math.random() * rows);
+
+    food = {x: randomX, y: randomY};
 
 }
 
